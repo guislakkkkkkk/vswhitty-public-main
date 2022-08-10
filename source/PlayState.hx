395b4fdaf29a7cfa6230cc20408f42504503f665
guislakkkkkkk <107629042+guislakkkkkkk@users.noreply.github.com>
2022-08-09 23:12:41 -0300

    Update PlayState.hx
    eu acho q tem bug aqui sla
    
    Signed-off-by: guislakkkkkkk <107629042+guislakkkkkkk@users.noreply.github.com>

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -55,6 +55,7 @@ import lime.utils.Assets;
 import openfl.display.BlendMode;
 import openfl.display.StageQuality;
 import openfl.filters.ShaderFilter;
+
 import openfl.utils.Assets as OpenFlAssets;
 
 #if windows
